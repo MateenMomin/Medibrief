6879614ecdf43cd88998bff43c2b0bff1dc9679a
Mateen <mateenmomin7840@gmail.com>
2026-03-01 16:02:05 +0530

    changes in code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,HTTPException,Depends,UploadFile,File
+from fastapi import FastAPI,HTTPException,Depends,UploadFile,File,Query
 from db import User,Reports,get_session_maker,create_tables_and_all,Reports
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
@@ -7,6 +7,9 @@ from authentication import current_active_user,fastapi_user,auth_backend
 import shutil
 import os
 from report import router as file_router
+from sqlalchemy import select
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
@@ -15,6 +18,15 @@ async def lifespan(app:FastAPI):
 
 
 app=FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
+ 
 app.include_router(
     fastapi_user.get_auth_router(auth_backend),
     prefix="/auth/jwt",
@@ -45,4 +57,36 @@ async def Text_Upload(
     
     return {"message":"Report saved successfully",
             "report_id":new_report.id}
+    
+
+@app.get("/reports")
+async def get_reports(
+    report_id: Optional[int] = Query(default=None),
+    session: AsyncSession = Depends(get_session_maker),
+    user=Depends(current_active_user)
+):
+    
+    if report_id is not None:
+        result = await session.execute(
+            select(Reports).where(
+                Reports.id == report_id
+            )
+        )
+        report = result.scalar_one_or_none()
+
+        if not report:
+            raise HTTPException(status_code=404, detail="Report not found")
+        
+        if report.user_id!=user.id:
+            raise HTTPException(status_code=403,detail="Report does Not Belong to you")
+
+        return report
+
+
+    result = await session.execute(
+        select(Reports).where(Reports.user_id == user.id)
+    )
+    reports = result.scalars().all()
+
+    return reports
     
